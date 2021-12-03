ceec49e59d06082b23265901dcac7d5ecd69fc15
ata <ataturkoglu@outlook.com.tr>
2021-12-03 18:23:50 +0300

    :recycle: refactor: modify folder structure

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,21 +1,25 @@
 <template>
   <v-app id="app">
-    <v-app-bar app color="transparent" dark flat align-center>
+    <v-app-bar app color="transparent" dark flat align-center hide-on-scroll>
       <v-app-bar-nav-icon
         v-if="$vuetify.breakpoint.xs"
         @click.stop="drawer = !drawer"
       ></v-app-bar-nav-icon>
-      <v-toolbar-title role="button"> <h2>Patika</h2> </v-toolbar-title>
+      <router-link v-slot="{ href, navigate }" to="/" custom>
+        <v-toolbar-title :href="href" @click="navigate" role="button">
+          <h2>Patika</h2>
+        </v-toolbar-title>
+      </router-link>
       <v-spacer></v-spacer>
       <v-container class="nav-router" v-if="$vuetify.breakpoint.smAndUp">
         <v-row justify="center">
           <v-col sm="6" md="4" lg="2" xl="2" class="p-0 d-flex justify-center">
-            <router-link v-slot="{href,navigate}" to="/" custom>
+            <router-link v-slot="{ href, navigate }" to="/dashboard" custom>
               <v-btn :href="href" @click="navigate" text> Dashboard </v-btn>
             </router-link>
           </v-col>
           <v-col sm="6" md="4" lg="2" xl="2" class="p-0 d-flex justify-center">
-            <router-link v-slot="{href,navigate}" to="/" custom>
+            <router-link v-slot="{ href, navigate }" to="/" custom>
               <v-btn :href="href" @click="navigate" text> User Settings </v-btn>
             </router-link>
           </v-col>
@@ -71,59 +75,84 @@
       </v-menu>
     </v-app-bar>
     <v-main>
-      <v-navigation-drawer v-if="$vuetify.breakpoint.xs" v-model="drawer" width="50%" absolute temporary color="transparent">
+      <v-navigation-drawer
+        v-if="$vuetify.breakpoint.xs"
+        v-model="drawer"
+        width="50%"
+        absolute
+        temporary
+        color="transparent"
+      >
         <v-list nav shaped>
           <v-list-item-group class="mt-6">
             <v-list-item>
-              <router-link v-slot="{href,navigate}" to="/" custom>
-                <span :href="href" @click="navigate" class="router-link-mobile"> <h3>Dashboard</h3> </span>
+              <router-link v-slot="{ href, navigate }" to="/dashboard" custom>
+                <span :href="href" @click="navigate" class="router-link-mobile">
+                  <h3>Dashboard</h3>
+                </span>
               </router-link>
             </v-list-item>
             <v-list-item>
-              <router-link v-slot="{href,navigate}" to="/" custom>
-                <span :href="href" @click="navigate" class="router-link-mobile"> <h3>User Settings</h3> </span>
+              <router-link v-slot="{ href, navigate }" to="/" custom>
+                <span :href="href" @click="navigate" class="router-link-mobile">
+                  <h3>User Settings</h3>
+                </span>
               </router-link>
             </v-list-item>
           </v-list-item-group>
         </v-list>
       </v-navigation-drawer>
-      <router-view />
+      <router-view style="z-index: 1" />
     </v-main>
+    <!--
     <div id="bg">
       <img src="@/assets/bg/bg(3).jpg">
-    </div>
+    </div>-->
   </v-app>
 </template>
 
 <script>
 export default {
-  name: "App",
+  name: 'App',
   data() {
     return {
-      lang: "tr",
-      drawer:false
-    };
+      lang: 'tr',
+      drawer: false,
+    }
   },
-};
+}
 </script>
 <style>
-
 #app {
   -ms-overflow-style: none;
   scrollbar-width: none;
+  background-image: url('assets/bg/bg(3).jpg');
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+}
+#app:after {
+  content: '';
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background: rgba(0, 0, 0, 0.55);
+  pointer-events: none;
 }
 ::-webkit-scrollbar {
   display: none;
 }
-
-#bg{
+#bg {
   position: absolute;
   left: 0;
   top: 0;
   height: 100%;
   width: 100%;
+  z-index: 0;
 }
-img{
+img {
   object-fit: cover;
   height: 100%;
   width: 100%;
@@ -132,10 +161,10 @@ img{
 .nav-router {
   width: 40%;
 }
-.router-link-mobile{
+.router-link-mobile {
   color: white;
 }
-.v-list-item--active{
-  background-color:grey;
+.v-list-item--active {
+  background-color: grey;
 }
 </style>
